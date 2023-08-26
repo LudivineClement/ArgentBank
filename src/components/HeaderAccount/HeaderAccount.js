fb09c7ed95350ac6ad9245fa659bbd228d827380
ludivineclement <ludivine.clement0985@gmail.com>
2023-08-26 19:22:01 +0200

    update-username

--- src/components/HeaderAccount/HeaderAccount.js
+++ src/components/HeaderAccount/HeaderAccount.js
@@ -1,11 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchUserProfile } from '../../actions/user.action';
-import './headerAccount.css';
+import './headerAccount.css'
+import EditName from '../EditName/EditName'; 
 
 const HeaderAccount = () => {
   const dispatch = useDispatch();
   const userProfile = useSelector((state) => state.user.userProfile);
+  const [isEditing, setIsEditing] = useState(false); 
 
   useEffect(() => {
     dispatch(fetchUserProfile());
@@ -13,9 +15,15 @@ const HeaderAccount = () => {
 
   return (
     <div className="header">
-      <h1>Welcome back<br />{userProfile.userName} !</h1>
-      <button className="edit-button">Edit Name</button>
-      <h2 className="sr-only">Accounts</h2>
+      {isEditing ? (
+        <EditName setIsEditing={setIsEditing} /> 
+      ) : (
+        <>
+          <h1>Welcome back<br />{userProfile.userName} !</h1>
+          <button className="edit-button" onClick={() => setIsEditing(true)}>Edit Name</button>
+          <h2 className="sr-only">Accounts</h2>
+        </>
+      )}
     </div>
   );
 };
