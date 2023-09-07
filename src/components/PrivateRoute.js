85f39fdc230fb1b0ae7c3db8722df37e5547ec5b
ludivineclement <ludivine.clement0985@gmail.com>
2023-09-07 08:51:16 +0200

    modification_privateRoute

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { Outlet, Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 
 const PrivateRoute = () => {
-  const isAuthenticated = useSelector(state => state.user.isAuthenticated);  
-  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
+  const token = localStorage.getItem("token");
+  return token ? <Outlet /> : <Navigate to="/login" />;
 };
 
 export default PrivateRoute;
